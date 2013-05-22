3081c0bcf75e27ef5afb3214e063b3b2ce7ee02f
dwax <davidwachs@hotmail.de>
2013-05-22 16:55:34 +0200

    modified Webservice

--- src/main/java/com/photobay/webservice/PhotoBayService.java
+++ src/main/java/com/photobay/webservice/PhotoBayService.java
@@ -11,18 +11,45 @@ public class PhotoBayService {
 	
 	@GET
 	@Path("{photographer}")
+	@Produces({"application/xml"})
 	public Response getPhotographer(@PathParam("photographer") int id)
 	{
 		Photographer photographer = PhotoBayRessourceManager.getPhotographer(id);
+		if(photographer == null)
+		{
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
 		return Response.ok(photographer).build();
 	}
 	
 	@GET
 	@Path("{pressAgency}")
+	@Produces({"application/xml"})
 	public Response getPressAgency(@PathParam("pressAgency") int id)
 	{
 		PressAgency pressAgency = PhotoBayRessourceManager.getPressAgency(id);
+		if(pressAgency == null)
+		{
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
 		return Response.ok(pressAgency).build();
 	}
 	
+	@PUT
+	@Path("{pressAgency}")
+	@Consumes({"application/xml"})
+	public Response putPressAgency(PressAgency pressAgency)
+	{
+		PhotoBayRessourceManager.putPressAgency(pressAgency);
+		return Response.ok().build();
+	}
+	
+	@PUT
+	@Path("{photographer}")
+	@Consumes({"application/xml"})
+	public Response putPhotographer(Photographer photographer)
+	{
+		PhotoBayRessourceManager.putPhotographer(photographer);
+		return Response.ok().build();
+	}
 }
