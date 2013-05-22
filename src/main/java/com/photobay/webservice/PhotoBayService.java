7700e8ebb4b925daecbbe39c9312c71aedcd483e
dwax <davidwachs@hotmail.de>
2013-05-22 16:45:05 +0200

    modfied Webservice

--- src/main/java/com/photobay/webservice/PhotoBayService.java
+++ src/main/java/com/photobay/webservice/PhotoBayService.java
@@ -1,5 +1,28 @@
 package main.java.com.photobay.webservice;
 
-public class PhotoBayService {
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import main.java.com.photobay.jaxbfiles.*;
 
+import main.java.com.photobay.webservice.PhotoBayRessourceManager;
+
+@Path("/host")
+public class PhotoBayService {
+	
+	@GET
+	@Path("{photographer}")
+	public Response getPhotographer(@PathParam("photographer") int id)
+	{
+		Photographer photographer = PhotoBayRessourceManager.getPhotographer(id);
+		return Response.ok(photographer).build();
+	}
+	
+	@GET
+	@Path("{pressAgency}")
+	public Response getPressAgency(@PathParam("pressAgency") int id)
+	{
+		PressAgency pressAgency = PhotoBayRessourceManager.getPressAgency(id);
+		return Response.ok(pressAgency).build();
+	}
+	
 }
