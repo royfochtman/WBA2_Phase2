dd5394204755c22d6fd12092589457e6b5a1adda
royfochtman <roy.fochtman@hotmail.com>
2013-05-29 16:09:22 +0200

    implementation of JAXB files

--- src/main/java/com/photobay/webservice/PhotoBayRessourceManager.java
+++ src/main/java/com/photobay/webservice/PhotoBayRessourceManager.java
@@ -15,7 +15,7 @@ import com.sun.xml.bind.v2.TODO;
 public class PhotoBayRessourceManager {
 	
 	/**
-	 * This Method returns the Photographer Object mit ID "id".
+	 * This Method returns the Photographer Object with ID "id".
 	 * 
 	 * @param id
 	 * @return Photographer
@@ -94,7 +94,7 @@ public class PhotoBayRessourceManager {
 		try
 		{
 			File file = new File("./host/pressAgencies/" + id + "/pressAgency.xml");
-			JAXBContext context = JAXBContext.newInstance("main.java.com.photobay.webservice");
+			JAXBContext context = JAXBContext.newInstance(PressAgency.class);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
 			return (PressAgency)unmarshaller.unmarshal(file);
 		}
@@ -114,11 +114,11 @@ public class PhotoBayRessourceManager {
 		try
 		{
 			int id = IdGenerator.generateID("pressAgencies");
-			File dir = new File("./host/pressAgency/" + id);
+			File dir = new File("./host/pressAgencies/" + id);
 			File file = new File(dir + "/PressAgency.xml");
 			if(dir.mkdir() && file.createNewFile())
 			{
-				JAXBContext context = JAXBContext.newInstance("main.java.com.photobay.webservice");
+				JAXBContext context = JAXBContext.newInstance(PressAgency.class);
 			    Marshaller m = context.createMarshaller();
 			    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 			    pressAgency.setID(id);
@@ -151,4 +151,152 @@ public class PhotoBayRessourceManager {
 		else return false;
 	}
 
+	//TODO
+	public static Boolean postBid(Bid bid) {
+//			try
+//			{
+//				int id = IdGenerator.generateID("bids");
+//				File dir = new File("./host/pressAgency/" + id);
+//				File file = new File(dir + "/PressAgency.xml");
+//				if(dir.mkdir() && file.createNewFile())
+//				{
+//					JAXBContext context = JAXBContext.newInstance("main.java.com.photobay.webservice");
+//				    Marshaller m = context.createMarshaller();
+//				    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+//				    pressAgency.setID(id);
+//				    m.marshal(pressAgency, file);
+//				    return true;
+//				}
+//				return false;
+//			}
+//			catch(Exception ex)
+//			{
+//				return false;
+//			}
+		return true;
+	}
+	//TODO
+	public static Bid getBid(int id) {
+		return null;
+	}
+	/**
+	 * 
+	 * @param job
+	 * @return
+	 */
+	public static Boolean postJob(Job job){
+			try
+			{
+				int id = IdGenerator.generateID("jobs");
+				File dir = new File("./host/jobs/" + id);
+				File file = new File(dir + "/job.xml");
+				if(dir.mkdir() && file.createNewFile())
+				{
+					JAXBContext context = JAXBContext.newInstance(Job.class);
+				    Marshaller m = context.createMarshaller();
+				    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+				    job.setID(id);
+				    m.marshal(job, file);
+				    return true;
+				}
+				return false;
+			}
+			catch(Exception ex)
+			{
+				return false;
+			}
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public static Job getJob(int id) {
+			try
+			{
+				File file = new File("./host/jobs/" + id + "/job.xml");
+				JAXBContext context = JAXBContext.newInstance(Job.class);
+				Unmarshaller unmarshaller = context.createUnmarshaller();
+				return (Job)unmarshaller.unmarshal(file);
+			}
+			catch(Exception ex)
+			{
+				return null;
+			}
+	}
+	
+	//TODO
+	public static Boolean postJobApplication(JobApplication jobApplication){
+		return true;
+	}
+	//TODO
+	public static JobApplication getjobApplication(int id) {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param photoSell
+	 * @return
+	 */
+	public static Boolean postPhotoSell(PhotoSell photoSell){
+		try
+		{
+			int id = IdGenerator.generateID("photoSells");
+			File dir = new File("./host/photoSells/" + id);
+			File file = new File(dir + "/photoSell.xml");
+			if(dir.mkdir() && file.createNewFile())
+			{
+				JAXBContext context = JAXBContext.newInstance(PhotoSell.class);
+			    Marshaller m = context.createMarshaller();
+			    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			    photoSell.setID(id);
+			    m.marshal(photoSell, file);
+			    return true;
+			}
+			return false;
+		}
+		catch(Exception ex)
+		{
+			return false;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public static PhotoSell getPhotoSell(int id) {
+		try
+		{
+			File file = new File("./host/photoSells/" + id + "/photoSell.xml");
+			JAXBContext context = JAXBContext.newInstance(PhotoSell.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			return (PhotoSell)unmarshaller.unmarshal(file);
+		}
+		catch(Exception ex)
+		{
+			return null;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public static Boolean deletePhotoSell(int id){
+		File dir = new File("./host/photoSellss/" + id);
+		File file = new File("./host/photoSell/" + id + "/photoSell.xml");
+		if(file.delete()){
+			if(dir.delete())
+				return true;
+			else
+				return false;
+		}
+		else return false;
+	}
+	
 }
