f952130bee700c833a67ffb67c3af21128420fae
royfochtman <roy.fochtman@hotmail.com>
2013-06-19 22:20:49 +0200

    minor changes on userframe

---
+++ src/main/java/com/photobay/util/DataValidator.java
@@ -0,0 +1,39 @@
+package main.java.com.photobay.util;
+
+import main.java.com.photobay.gui.UserFrame;
+
+public class DataValidator {
+	
+	public static Boolean validateJobData(){
+		return false;
+	}
+	
+	public static Boolean validatePhotoData(){
+		return false;
+	}
+	
+	public static Boolean validateBidData(){
+		return false;
+	}
+	
+	public static Boolean validateGeneralPersonalData(){
+		return false;
+	}
+	
+	public static Boolean validateJobAplicationData(){
+		return false;
+	}
+	
+	public static Boolean validatePhotoSellData(){
+		return false;
+	}
+	
+	public static Boolean validatePhotographerData(){
+		return false;
+	}
+	
+	public static Boolean validatePressAgencyData(){
+		return false;
+	}
+
+}
