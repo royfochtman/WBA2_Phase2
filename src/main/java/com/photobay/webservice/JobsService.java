b760fe42b823f34c140ee2b40b1102ceb3ce3c1c
royfochtman <roy.fochtman@hotmail.com>
2013-06-22 17:25:49 +0200

    trying put job

--- src/main/java/com/photobay/webservice/JobsService.java
+++ src/main/java/com/photobay/webservice/JobsService.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -43,6 +44,21 @@ public class JobsService {
 		}
 		else return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 	}
+	
+	/*Testing Post Job*/
+	@PUT
+	@Consumes({"application/xml"})
+	public Response putJob(Job job)
+	{
+		
+		if(PhotoBayRessourceManager.putJob(job))
+		{
+			return Response.ok().build();
+		}
+		else return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+	}
+	/**/
+	
 	
 	@GET
 	@Path("/query")
